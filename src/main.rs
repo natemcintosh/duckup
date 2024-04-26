a3b1c4625a074cdd5305de588b9dfc2753d7bd4b
Nate McIntosh <natemcintosh@gmail.com>
2024-04-26 17:05:41 -0400

    closer still

--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,190 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use reqwest::blocking::Client;
+use reqwest::{blocking::Client, header::USER_AGENT};
 use tempfile::tempdir;
 use zip::read::ZipArchive;
 
-#[derive(Deserialize)]
-struct Release {
-    assets: Vec<Asset>,
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Release {
+    pub url: String,
+    #[serde(rename = "assets_url")]
+    pub assets_url: String,
+    #[serde(rename = "upload_url")]
+    pub upload_url: String,
+    #[serde(rename = "html_url")]
+    pub html_url: String,
+    pub id: i64,
+    pub author: Author,
+    #[serde(rename = "node_id")]
+    pub node_id: String,
+    #[serde(rename = "tag_name")]
+    pub tag_name: String,
+    #[serde(rename = "target_commitish")]
+    pub target_commitish: String,
+    pub name: String,
+    pub draft: bool,
+    pub prerelease: bool,
+    #[serde(rename = "created_at")]
+    pub created_at: String,
+    #[serde(rename = "published_at")]
+    pub published_at: String,
+    pub assets: Vec<Asset>,
+    #[serde(rename = "tarball_url")]
+    pub tarball_url: String,
+    #[serde(rename = "zipball_url")]
+    pub zipball_url: String,
+    pub body: String,
+    pub reactions: Reactions,
+    #[serde(rename = "mentions_count")]
+    pub mentions_count: i64,
 }
 
-#[derive(Deserialize)]
-struct Asset {
-    browser_download_url: String,
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Author {
+    pub login: String,
+    pub id: i64,
+    #[serde(rename = "node_id")]
+    pub node_id: String,
+    #[serde(rename = "avatar_url")]
+    pub avatar_url: String,
+    #[serde(rename = "gravatar_id")]
+    pub gravatar_id: String,
+    pub url: String,
+    #[serde(rename = "html_url")]
+    pub html_url: String,
+    #[serde(rename = "followers_url")]
+    pub followers_url: String,
+    #[serde(rename = "following_url")]
+    pub following_url: String,
+    #[serde(rename = "gists_url")]
+    pub gists_url: String,
+    #[serde(rename = "starred_url")]
+    pub starred_url: String,
+    #[serde(rename = "subscriptions_url")]
+    pub subscriptions_url: String,
+    #[serde(rename = "organizations_url")]
+    pub organizations_url: String,
+    #[serde(rename = "repos_url")]
+    pub repos_url: String,
+    #[serde(rename = "events_url")]
+    pub events_url: String,
+    #[serde(rename = "received_events_url")]
+    pub received_events_url: String,
+    #[serde(rename = "type")]
+    pub type_field: String,
+    #[serde(rename = "site_admin")]
+    pub site_admin: bool,
 }
 
-fn get_latest_release_zip_url(
-    owner: &str,
-    repo: &str,
-) -> Result<String, Box<dyn std::error::Error>> {
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Asset {
+    pub url: String,
+    pub id: i64,
+    #[serde(rename = "node_id")]
+    pub node_id: String,
+    pub name: String,
+    pub label: String,
+    pub uploader: Uploader,
+    #[serde(rename = "content_type")]
+    pub content_type: String,
+    pub state: String,
+    pub size: i64,
+    #[serde(rename = "download_count")]
+    pub download_count: i64,
+    #[serde(rename = "created_at")]
+    pub created_at: String,
+    #[serde(rename = "updated_at")]
+    pub updated_at: String,
+    #[serde(rename = "browser_download_url")]
+    pub browser_download_url: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Uploader {
+    pub login: String,
+    pub id: i64,
+    #[serde(rename = "node_id")]
+    pub node_id: String,
+    #[serde(rename = "avatar_url")]
+    pub avatar_url: String,
+    #[serde(rename = "gravatar_id")]
+    pub gravatar_id: String,
+    pub url: String,
+    #[serde(rename = "html_url")]
+    pub html_url: String,
+    #[serde(rename = "followers_url")]
+    pub followers_url: String,
+    #[serde(rename = "following_url")]
+    pub following_url: String,
+    #[serde(rename = "gists_url")]
+    pub gists_url: String,
+    #[serde(rename = "starred_url")]
+    pub starred_url: String,
+    #[serde(rename = "subscriptions_url")]
+    pub subscriptions_url: String,
+    #[serde(rename = "organizations_url")]
+    pub organizations_url: String,
+    #[serde(rename = "repos_url")]
+    pub repos_url: String,
+    #[serde(rename = "events_url")]
+    pub events_url: String,
+    #[serde(rename = "received_events_url")]
+    pub received_events_url: String,
+    #[serde(rename = "type")]
+    pub type_field: String,
+    #[serde(rename = "site_admin")]
+    pub site_admin: bool,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Reactions {
+    pub url: String,
+    #[serde(rename = "total_count")]
+    pub total_count: i64,
+    #[serde(rename = "+1")]
+    pub n1: i64,
+    #[serde(rename = "-1")]
+    pub n12: i64,
+    pub laugh: i64,
+    pub hooray: i64,
+    pub confused: i64,
+    pub heart: i64,
+    pub rocket: i64,
+    pub eyes: i64,
+}
+
+fn get_latest_release_zip_urls() -> Result<Vec<String>, Box<dyn std::error::Error>> {
     // Construct the URL for GitHub API to fetch the latest release information
     let url = "https://api.github.com/repos/duckdb/duckdb/releases/latest";
 
     // Send a GET request to fetch the latest release information
-    let response = reqwest::blocking::get(url)?;
-    let release: Release = response.json()?;
+    let client = Client::new();
+    let text = client
+        .get(url)
+        .header(USER_AGENT, "duckup")
+        .send()?
+        .text()?;
+    let release: Release = serde_json::from_str(&text)?;
 
-    // Extract the URL of the zip file from the release information
-    if let Some(asset) = release
+    // Extract the URL of the zip files from the release information
+    Ok(release
         .assets
-        .into_iter()
-        .find(|asset| asset.browser_download_url.ends_with(".zip"))
-    {
-        Ok(asset.browser_download_url)
-    } else {
-        Err("No zip file found in the latest release".into())
-    }
+        .iter()
+        .map(|a| a.browser_download_url.clone())
+        .collect())
 }
 
-fn download_zip(url: &str) -> Result<PathBuf, Box<dyn std::error::Error>> {
+fn download_zip(url: &str, output_dir: &Path) -> Result<PathBuf, Box<dyn std::error::Error>> {
+    dbg!(output_dir.is_dir());
+
     // Create a reqwest client
     let client = Client::new();
 
@@ -53,17 +199,17 @@ fn download_zip(url: &str) -> Result<PathBuf, Box<dyn std::error::Error>> {
     }
 
     // Open a file to write the downloaded content
-    let temp_dir = tempdir()?;
-    let zip_file_path = temp_dir.path().join("downloaded.zip");
+    let zip_file_path = output_dir.join("downloaded.zip");
     let mut file = File::create(&zip_file_path)?;
 
     // Copy the content of the response to the file
     let _ = response.copy_to(&mut file);
-
+    dbg!(&zip_file_path.exists());
     Ok(zip_file_path)
 }
 
 fn unzip_file(zip_file: &Path, output_dir: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    dbg!(&zip_file.exists());
     let file = File::open(zip_file)?;
     let mut archive = ZipArchive::new(file)?;
 
@@ -88,19 +234,46 @@ fn unzip_file(zip_file: &Path, output_dir: &Path) -> Result<(), Box<dyn std::err
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url =
-        "https://github.com/duckdb/duckdb/releases/download/v0.10.2/duckdb_cli-linux-amd64.zip";
+fn get_matching_url<'a>(
+    zip_urls: &'a [String],
+    info: &'a os_info::Info,
+) -> Result<&'a str, Box<dyn std::error::Error>> {
+    Ok(zip_urls
+        .iter()
+        // Filter to urls with this OS
+        .filter(|url| url.contains("linux"))
+        // Only want the CLI binaries
+        .filter(|url| url.contains("cli"))
+        // Filter to the correct architecture
+        .filter(|url| match info.architecture() {
+            Some("arm64") | Some("aarch") => url.contains("aarch"),
+            Some("amd") | Some("x86") => url.contains("amd"),
+            Some(&_) | None => false,
+        })
+        .next()
+        .ok_or("Could not find any matching URLs")?)
+}
 
-    let zip_file_path = download_zip(url)?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let info = os_info::get();
+    let zip_urls: Vec<String> = get_latest_release_zip_urls()?.into_iter().collect();
+    // Get the correct url for this architecture
+    let url = get_matching_url(&zip_urls, &info)?;
+    println!("Going to download {}", url);
+
+    // Create a temp directory for unzipping
+    let zip_dir = tempdir()?;
+    std::fs::create_dir_all(zip_dir.path())?;
+    // Download the file
+    let zip_file_path = download_zip(url, &zip_dir.path())?;
+    dbg!(&zip_file_path.exists());
 
     println!("Downloaded successfully!");
 
-    // Create a temp directory for unzipping
-    let temp_dir = tempdir()?;
-    let output_dir = temp_dir.path().join("output");
-    unzip_file(&zip_file_path, &output_dir)?;
-    println!("Unzipped successfully!");
+    let final_path = Path::new("~/Desktop/bin/");
+    std::fs::create_dir_all(final_path)?;
+    unzip_file(&zip_file_path, &final_path)?;
+    println!("Unzipped successfully into {:?}", final_path);
 
     Ok(())
 }
